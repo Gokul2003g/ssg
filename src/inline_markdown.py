ea50c0828d99b1f7620836fe58444cf2df8a385e
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2025-09-26 20:50:45 +0530

    Using solution as stuck at this stage.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -101,5 +101,3 @@ def text_to_textnodes(text):
     #     print(node)
     # print()
     return nodes
-
-text_to_textnodes("This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)")
